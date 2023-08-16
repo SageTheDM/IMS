220de895559e5a429945f72d190577c7f4a44f85
Sage Burger <luca.burger@grischuni.net>
2023-08-16 15:11:44 +0200

    Commented my code

--- src/Grundlagen.java
+++ src/Grundlagen.java
@@ -1,21 +1,27 @@
 public class Grundlagen {
     public static void main(String[] args) {
+        // Hello World
         System.out.println("Hello World");
 
+        // Zahlen Variablen
         int x = 99;
         int y = 2;
         x = x + 1;
         x = x + y;
         System.out.println(x);
 
+        // Buchstaben Variablen
         char myA = 'a';
+        System.out.println(myA);
 
+        // If & else
         if (x > 101) {
             System.out.println("Der Wert ist grösser als 101");
         } else {
             System.out.println("Der Wert ist kleiner als 101");  
         }
 
+        // Iteration
         int anzahlDurchgaenge = 1;
         while(anzahlDurchgaenge <= 5) {
             System.out.println("Durchgang nr. " + anzahlDurchgaenge);
