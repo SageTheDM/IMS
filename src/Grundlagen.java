784ca4113f1667237c635b1891127336df038a0d
Sage Burger <luca.burger@grischuni.net>
2023-08-16 14:52:04 +0200

    Grundlagen Variabeln

--- src/Grundlagen.java
+++ src/Grundlagen.java
@@ -1,6 +1,11 @@
 public class Grundlagen {
-    public static void main(String[] args)
-    {
-        System.out.println("Test");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int x = 99;
+        int y = 1;
+        x = x + 1;
+        x = x + y;
+        System.out.println(x);
     }
 }
