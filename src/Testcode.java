b59fe4b9a1daf7e405a26b0e40e072e9d71d5da7
Sage Burger <luca.burger@grischuni.net>
2023-08-22 10:26:08 +0200

    long > int >  short > byte

--- src/Testcode.java
+++ src/Testcode.java
@@ -1,5 +1,11 @@
 public class Testcode {
     public static void main(String[] args) {
 
+        int a = 1, b = 2, c = 3, d = 4, e = 5, f = 6, g = 7, h = 8;
+        int sum = a + b + c + d + e + f + g + h;
+
+        System.out.println(sum);
+        byte x = 127;
+
     }
 }
