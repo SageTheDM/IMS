df0fabee6f1609122d2bee89186628a3053c3b4a
Sage The DM <luca.burger@grischuni.net>
2024-10-22 09:56:16 +0200

    Copied the teacher

--- Code/ost/_01_hello_world/src/main/java/com/example/App.java
+++ Code/ost/_01_hello_world/src/main/java/com/example/App.java
@@ -3,26 +3,22 @@ package com.example;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-
-/**
- * JavaFX App
- */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
+        Label label = new Label("Hello World");
+        label.setFont(new Font(40));
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        var scene = new Scene(new StackPane(label), 640, 480);
+        Scene scene = new Scene(label);
+
         stage.setScene(scene);
+        stage.setTitle("Welcome to JavaFX!");
         stage.show();
     }
-
     public static void main(String[] args) {
         launch();
     }
